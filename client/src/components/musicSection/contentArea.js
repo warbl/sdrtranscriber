829e8c542ba02d4879f38c1cf3ecf620a0680f47
Aryanna <tuh05038@temple.edu>
2021-03-11 17:44:06 -0500

    working on UI functionality

--- client/src/components/musicSection/contentArea.js
+++ client/src/components/musicSection/contentArea.js
@@ -1,10 +1,10 @@
-import React, {useState, useEffect} from "react";
-import {Form} from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Form } from 'react-bootstrap';
 import Axios from 'axios';
 import "./musicSection.css";
 
 
-export default function ContentArea({station}){
+export default function ContentArea({ station }) {
     const [songs, setSongs] = useState([]);
     const [filterResults, setFilterResults] = useState([]);
 
@@ -13,24 +13,26 @@ export default function ContentArea({station}){
     }, [station]);
 
     const fetchSongsByStation = (station) => {
-        const stationFreq = station.station_freq
+        document.body.style.cursor = 'wait';
+        const stationFreq = station.station_freq;
         Axios.get("http://localhost:3001/api/getSongsByStation/" + stationFreq).then((response) => {
             console.log("getting songs from station: " + stationFreq);
             const data = response.data;
-            if(data.length > 0){
-            data.forEach(element => {
-                const newDate = formatDate(element.time_played);
-                element.time_played = newDate;
-            });
-        }
-        console.log(data);
-        setSongs(data);
-        setFilterResults(data);
-        //sleep();
+            if (data.length > 0) {
+                data.forEach(element => {
+                    const newDate = formatDate(element.time_played);
+                    element.time_played = newDate;
+                });
+            }
+            console.log(data);
+            setSongs(data);
+            setFilterResults(data);
+            document.body.style.cursor = 'default';
         }).catch((error) => {
             console.log(error);
+            document.body.style.cursor = 'wait';
         });
-    }; 
+    };
 
     const formatDate = (oldDate) => {
         console.log(oldDate);
@@ -39,14 +41,14 @@ export default function ContentArea({station}){
         //date
         let date = data[0].split("-");
         const year = date[0];
-        const month = months[parseInt(date[1])-1];
+        const month = months[parseInt(date[1]) - 1];
         const day = date[2];
         date = month + " " + day + " " + year;
         //time
         let time = data[1].split(":");
         let hour = parseInt(time[0]);
         // UTC to EST
-        if(hour > 5){
+        if (hour > 5) {
             hour = hour - 5;
         } else {
             let tempHour = 24;
@@ -54,14 +56,14 @@ export default function ContentArea({station}){
             hour = tempHour - timeLeft;
         }
         let pm = false;
-        if(hour === 12){
+        if (hour === 12) {
             pm = true;
         }
-        if(hour > 12){
+        if (hour > 12) {
             hour = hour - 12;
-            if(hour === 12){
+            if (hour === 12) {
                 pm = false;
-            }else{
+            } else {
                 pm = true;
             }
         }
@@ -72,57 +74,62 @@ export default function ContentArea({station}){
         return date + " at " + time;
     }
 
-    /*
-    const sleep = async() => {
+
+    const sleep = async () => {
         const tempStation = station;
         await new Promise(r => setTimeout(r, 60000));
         console.log("slept!");
-        fetchSongsByStation(tempStation);
     }
-    */
 
     const search = (e) => {
         const search = e.target.value;
-        const result = songs.filter(value =>  value.song_name.toLowerCase().includes(search.toLowerCase()) || value.song_artist.toLowerCase().includes(search));
+        const result = songs.filter(value => value.song_name.toLowerCase().includes(search.toLowerCase()) || value.song_artist.toLowerCase().includes(search));
         setFilterResults(result);
     };
 
-    return(
+    const refresh = () => {
+        fetchSongsByStation(station);
+    }
+
+    return (
         <>
-        <div className="container">
-        <h1 className="stationHeading"> {station.station_name} - {station.station_freq} SET LIST </h1>
-        {songs.length > 0 &&
-        <Form className="filter-form-songs">
-                <Form.Group className="filter-form-song-box">
-                    <Form.Control onChange={search} className='filter-form-song-input' type="text" placeholder=" Search songs..." />
-                </Form.Group>
-            </Form>
-            }
-        {filterResults.length <= 0 ? (
-                 <div className="empty_song_container">
-                     <span>¯\_(ツ)_/¯</span>
-                     <p>Sorry no songs could be found</p>
-                </div>
-        ) : (
-            <div className="song_container" id='scrollbar'>
-                    {filterResults && filterResults.map((val, index) => {
-                        return(
-                        <div className="song" key={val.song_id}>
-                            <div className="album-image">
-                                <img src={val.album_cover} alt="album_image"/>
-                            </div>
-                            <div className="song-info">
-                            <h1>{index+1}. {val.song_name}</h1>
-                            <h3>Artist: {val.song_artist}</h3>
-                            <h3>Played on: {val.time_played}</h3>
-                            <a href={val.yt_link} target="_blank" rel="noreferrer" className="yt-link">Listen to Song HERE</a>
-                            </div>
+            <div className="container">
+                <h1 className="stationHeading"> {station.station_name} - {station.station_freq} SET LIST </h1>
+                {songs.length > 0 &&
+                    <div>
+                        <button className="refresh" onClick={refresh}>REFRESH</button>
+                        <Form className="filter-form-songs">
+                            <Form.Group className="filter-form-song-box">
+                                <Form.Control onChange={search} className='filter-form-song-input' type="text" placeholder=" Search songs..." />
+                            </Form.Group>
+                        </Form>
+                    </div>
+                }
+                {filterResults.length <= 0 ? (
+                    <div className="empty_song_container">
+                        <span>¯\_(ツ)_/¯</span>
+                        <p>Sorry no songs could be found</p>
+                    </div>
+                ) : (
+                        <div className="song_container" id='scrollbar'>
+                            {filterResults && filterResults.map((val, index) => {
+                                return (
+                                    <div className="song" key={val.song_id}>
+                                        <div className="album-image">
+                                            <img src={val.album_cover} alt="album_image" />
+                                        </div>
+                                        <div className="song-info">
+                                            <h1>{index + 1}. {val.song_name}</h1>
+                                            <h3>Artist: {val.song_artist}</h3>
+                                            <h3>Played on: {val.time_played}</h3>
+                                            <a href={val.yt_link} target="_blank" rel="noreferrer" className="yt-link">Listen to Song HERE</a>
+                                        </div>
+                                    </div>
+                                )
+                            })}
                         </div>
-                    )
-                })} 
+                    )}
             </div>
-        )}
-        </div>
         </>
     )
 }
--- client/src/components/navBar/navbar.js
+++ client/src/components/navBar/navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./navbar.css";
-import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import hamburger from '../../images/hamburger.png'
 
 
 
 export default function NavBar(){
+	const history = useHistory();
 	
 			
 		function displayTabs(){
@@ -17,10 +18,16 @@ export default function NavBar(){
 				x.className = "nav";
 			}
 		}
+
+		const goToHomePage = () => {
+			history.push({
+				pathname: '/',
+			});
+		}
 	
     return(
         <div className="nav" id = "topNav">
-            <div className="navTitle">
+            <div className="navTitle" onClick={goToHomePage}>
                 SDR Transcriber
             </div>
             <div className="navTabs">
