c2809e7d758aab9587b4591ba90eeab841624e65
Aryanna <tuh05038@temple.edu>
2021-03-17 18:26:54 -0400

    added new endpoints to server

--- server/index.js
+++ server/index.js
@@ -40,6 +40,34 @@ app.get("/api/getSongsByStation/:stationFreq", (req, res) => {
     })
 });
 
+app.get("/api/getLatestSong", (req, res) => {
+    const  getLatestSong = `SELECT * FROM song_played ORDER BY song_id DESC LIMIT 1`;
+    db.query(getLatestSong, (err, result) => {
+        console.log(result);
+        console.log(err);
+        res.send(result);
+    })
+});
+
+app.get("/api/getLatestSongByStation/:stationFreq", (req, res) => {
+    const stationFreq = req.params.stationFreq;
+    const  getLatestSongByStation = `SELECT * from song_played WHERE station_freq = ${stationFreq} ORDER BY song_id DESC LIMIT 1`;
+    db.query(getLatestSongByStation, (err, result) => {
+        console.log(result);
+        console.log(err);
+        res.send(result);
+    })
+});
+
+app.get("/api/getSongsByPopularity", (req, res) => {
+    const  getSongsByPopularity = `SELECT * FROM song_played WHERE song_id IN (SELECT MAX(song_id) FROM song_played GROUP BY song_name, song_artist) AND popularity_rating IS NOT NULL ORDER BY popularity_rating`;
+    db.query(getSongsByPopularity, (err, result) => {
+        console.log(result);
+        console.log(err);
+        res.send(result);
+    })
+});
+
 app.listen(3001, () => {
     console.log("SERVER IS RUNNING ON PORT 3001!");
 })
