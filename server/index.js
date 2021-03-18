c38492c8cabbef145f32a5555416d1b197b3b0a8
Aryanna <tuh05038@temple.edu>
2021-03-17 21:56:16 -0400

    also changing server to run locally

--- server/index.js
+++ server/index.js
@@ -58,6 +58,6 @@ app.get("/api/getSongsByPopularity", (req, res) => {
     })
 });
 
-app.listen((3001, '0.0.0.0'), () => {
+app.listen((3001), () => {
     console.log("SERVER IS RUNNING ON PORT 3001!");
 })
