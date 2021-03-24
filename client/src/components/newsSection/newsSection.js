10d1acb0fca3ace820ca5c96579f9765532ecf5b
MarcFrattarelli <59966859+MarcFrattarelli@users.noreply.github.com>
2021-03-24 15:04:37 -0400

    Implementing the connection to the news table in the database

--- client/src/components/newsSection/newsSection.js
+++ client/src/components/newsSection/newsSection.js
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
 import "./newsSection.css";
 
 
-export default function newsSection(){
+
+export default function NewsSection(){
+    const [newsContent, setNewsContent] = useState();
+
+    useEffect(() => {
+        fetchNewsContent();
+        const id = setInterval(fetchNewsContent, 10000);
+        return () => clearInterval(id);
+    }, []);
+
+    const fetchNewsContent= () => {
+        Axios.get("http://localhost:3001/api/getNewsContent").then((response) => {
+            setNewsContent(response.data);
+            console.log(response.data);
+        }).catch((error) => {
+            console.log(error);
+        })
+    };
+
     return(
         <div className="news">
-           <h1 className="title">News Section</h1>
+           <h1 className="title">Here is the Transcribed News Report from KYW NEWS</h1>
+           <div className="news_container">
+                    {newsContent && newsContent.map((val) => {
+                        return (
+                            <span className="news-section" key={val.time_of_broadcast}>
+                                {val.transcribed_speech}&nbsp;
+                            </span>
+                        )
+                    })}
+                </div>
         </div>
     )
 }
