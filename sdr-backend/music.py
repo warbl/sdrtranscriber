0b17d752faa9518070249f95d4d3dbd2b57146cd
warbl <59927247+warbl@users.noreply.github.com>
2021-05-11 19:15:18 -0400

    Update music.py

--- sdr-backend/music.py
+++ sdr-backend/music.py
@@ -5,7 +5,7 @@ import requests
 
 def musicRecog(music):
     data = {
-        'api_token': 'a7be9aa12f182b7ef4ad00ae7a7d8026',
+        'api_token': '',
         'return': 'spotify',
     }
     files = {
@@ -19,8 +19,8 @@ def musicRecog (music):
     config = {
         #Replace "xxxxxxxx" below with your project's host, access_key and access_secret.
         'host':'identify-us-west-2.acrcloud.com',
-        'access_key':'939e967af1cdba363d503ff9396e0b5f', 
-        'access_secret':'F2InWkrpw4lzaqKXHpnsB4djaDDJPxtSff5ST6nN',
+        'access_key':'', 
+        'access_secret':'',
         'timeout':10 # seconds
     }
 
